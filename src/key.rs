ea3f9aaaad9cab334fb6fc6a53d7ff7f570367b2
Markkyn <marcos.gabriel@ossirian.com>
2025-04-07 21:35:12 -0300

    new: adding rcon

--- src/key.rs
+++ src/key.rs
@@ -1,6 +1,5 @@
 use crate::sbox_parser::Sbox;
 
-
 pub struct Key {
     pub key: u128,
     pub word_count: u8, // count of words
@@ -61,18 +60,20 @@ pub fn key_expansion( key : Key , nk : usize, n_rounds : usize, sbox : Sbox) {
     // for round
     for round in 0 .. n_rounds {
         
-        let mut words: [u32; 4] = key.to_vec_of_word();
-        
+        let mut words: [Word; 4] = key.to_vec_of_word();
+
+
         // Operation over the last word
         let last : usize = words.len() - 1;
 
         rot_word(&mut words[last], 1);
         sub_word(&mut words[last], &sbox);
-        r_con(&mut words[last]);
+        r_con(&mut words[last], round.try_into().unwrap());
 
         // XOR operation over all the words\
         // for word in key
         for ( i, word ) in words.iter().enumerate() {
+
         }
     
     }
@@ -80,10 +81,10 @@ pub fn key_expansion( key : Key , nk : usize, n_rounds : usize, sbox : Sbox) {
     () // return nothing
 }
 
-fn rot_word(word: &mut u32, t: usize ) { 
+fn rot_word(word: &mut Word, t: usize ) { 
     let bytes = word.to_be_bytes();
 
-    let mut temp: [u8; 4] = [0; 4];  
+    let mut temp: [Byte; 4] = [0; 4];  
     /*  t = 1 (eg.)
         
         [99, cf, 4f, 3c] = bytes
@@ -109,8 +110,8 @@ fn rot_word(word: &mut u32, t: usize ) {
     println!("     after rotation {:#02x}", word);
 }
 
-fn sub_word(word: &mut u32, sbox: &Sbox) {
-    let mut temp : [ u8; 4 ] = [0;4];
+fn sub_word(word: &mut Word, sbox: &Sbox) {
+    let mut temp : [ Byte; 4 ] = [0;4];
 
     println!("Word before substitution {:#02x}", word);
 
@@ -123,6 +124,20 @@ fn sub_word(word: &mut u32, sbox: &Sbox) {
     println!("     after substitution {:#02x}", word);
 }
 
-fn r_con(word: &u32) {
-    ()
+fn r_con(word: &mut Word, i_round: usize){
+    const RCON : [u8; 10] = [
+        0x01, 0x02, 0x04, 0x08, 0x10,
+        0x20, 0x40, 0x80, 0x1B, 0x36 
+    ];
+
+    let mut bytes = word.to_be_bytes();
+
+    println!("Rcon: {:#02x} -> {:#02x}", bytes[0], RCON[i_round]);
+    bytes[0] ^= RCON[i_round];
+
+    
+    *word = u32::from_be_bytes(bytes);
+
+    println!("\t after rcon: {:#02x}", word);
+
 }
