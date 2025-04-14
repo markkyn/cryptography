238bfdaf37f06711edaa401ab0cbd332f7c817cd
Markkyn <marcos.gabriel@ossirian.com>
2025-04-13 23:03:48 -0300

    new: key per round using pop

--- src/cipher.rs
+++ src/cipher.rs
@@ -1,31 +1,31 @@
 use crate::Block;
 use crate::Key;
 
-pub fn cipher(input : &Block, roundKeys: Vec<Key>) -> Block {
+pub fn cipher(input : &Block, mut roundKeys: Vec<Key>) -> Block {
 
-    let mut state : Block = *input;
+    let mut state : &Block = &input.clone();
 
     // pre-round
-    state = add_round_key(&input, roundKeys[0].pop());
+    state = add_round_key(&input, roundKeys.pop().expect("Couldnt pop round key"));
 
     // for round
     for r in 1..9 {
 
-        let key = roundKeys[r];
+        let key = roundKeys.pop()
+                .expect("Couldnt pop round key");
 
         //state = sub_bytes(state);
         //state = shift_rows(state);
         //state = mix_columns(state);
-        state = add_round_key(&state, key.pop());
+        state = add_round_key(&state, key);
     }
 
-    // last round
+    // last round - 11
     //state = sub_bytes(state);
     //state = shift_rows(state);
-    state = add_round_key(&roundKeys[0], kay.pop());
+    state = add_round_key(input, roundKeys.pop().expect("Couldnt pop round key"));
 
-
-    return state;
+    return state.clone();
 }
 
 fn add_round_key(input : &Block, key : Key) -> Block {
@@ -42,14 +42,13 @@ fn add_round_key(input : &Block, key : Key) -> Block {
         // Block Size: [u8; 16] = 128 bits of data
 
         // 1:1 Xor
-        data[i] = input.data[i] ^ key.key.to_be_bytes[i];
+        data[i] = input.data[i] ^ key.key.to_be_bytes()[i];
     }
 
-    let output : Block = Block {
-        data: data,
+    // returns the State Block = Input XOR Key
+    Block {
+        data: data.to_vec(),
         rows: 4,
         cols: 4
     };
-
-    return output;
 }
