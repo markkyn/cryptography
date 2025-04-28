9ae18c0e8efc4550adac7425083432cad9505cbd
Markkyn <marcos.gabriel@ossirian.com>
2025-04-28 06:01:10 -0300

    new: convert polinomial to u16 ( u8+carry = u9)

--- src/algebra.rs
+++ src/algebra.rs
@@ -1,3 +1,4 @@
+
 fn get_polinomial(x: u8) -> Vec<usize> {
     /*
         Returns: A Vector of indices of the bits with value 1;
@@ -6,7 +7,7 @@ fn get_polinomial(x: u8) -> Vec<usize> {
     */
     
     
-    let mut indexes = vec![usize;8]
+    let mut indexes = vec![0 as usize;8];
 
     for i in 0..8 {
         if ( x >> i ) & 0x01 == 0x01 {
@@ -15,4 +16,21 @@ fn get_polinomial(x: u8) -> Vec<usize> {
     }
 
     indexes
+}
+
+
+fn from_polinomial(pol_vec : Vec<usize>) -> u16 {
+
+    // it returns a number bigger than 2^7, thats why we returns a u16 instead of u8.
+    // Later we need to subtract by m(x) [Reduced Polinomial] to fit our result 
+    // in 8 bits
+
+
+    let mut result : u16 = 0;
+
+    for (i, pol) in pol_vec.into_iter().enumerate() {
+        result = (pol << i) as u16 
+    }
+
+    result
 }
